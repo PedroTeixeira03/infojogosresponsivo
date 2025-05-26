2d4a20a08fe14c86104f73928ae578638a8d78b3
PedroTeixeira03 <teixeirapedro03@gmail.com>
2025-05-26 12:55:10 +0100

    26-05-2025
    Atualização da BD da PAF

--- bdpaf.sql
+++ bdpaf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Abr-2025 às 14:16
+-- Tempo de geração: 26-Maio-2025 às 13:52
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.2.12
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `carrinho_compra`
+--
+
+CREATE TABLE `carrinho_compra` (
+  `n_carro` int(11) NOT NULL,
+  `n_sessao` varchar(255) NOT NULL,
+  `n_produto` int(11) NOT NULL,
+  `n_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `carrinho_compra`
+--
+
+INSERT INTO `carrinho_compra` (`n_carro`, `n_sessao`, `n_produto`, `n_user`) VALUES
+(1, 'sess_6834451d537f50.00417347', 12, 2),
+(3, 'sess_6834451d537f50.00417347', 15, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `plataforma`
 --
 
@@ -31,6 +52,15 @@ CREATE TABLE `plataforma` (
   `n_plataforma` int(11) NOT NULL,
   `plataforma` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `plataforma`
+--
+
+INSERT INTO `plataforma` (`n_plataforma`, `plataforma`) VALUES
+(1, 'PS5'),
+(2, 'XBOX'),
+(3, 'PC');
 
 -- --------------------------------------------------------
 
@@ -45,8 +75,18 @@ CREATE TABLE `produtos` (
   `n_plataforma` int(100) NOT NULL,
   `preco` float NOT NULL,
   `descricao` varchar(256) NOT NULL,
-  `imagem` longblob NOT NULL
+  `imagem` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`n_produto`, `marca`, `modelo`, `n_plataforma`, `preco`, `descricao`, `imagem`) VALUES
+(12, 'PLAYSTATION', 'JG. PS5 ASTRO BOT', 1, 69.99, 'Prepara-te para uma aventura emocionante com o jogo Astro Bot! Este título exclusivo para a PlayStation 5', 'fotos/1747666925_1746625551_img1.jpg'),
+(14, 'NAMCO-BANDAI', 'JG. PS5 ASSASSINS CREED SHADOW', 1, 79.99, 'Vive uma história épica de ação e aventura históricas passada no Japão feudal! ', 'fotos/1747668031_img2.jpg'),
+(15, 'MICROSOFT', ' Forza Horizon 5', 2, 69.99, 'A derradeira aventura do Horizon está à tua espera! Explora um mundo aberto de vibrantes paisagens mexicanas em constante evolução.', 'fotos/1747668595_img3.jpg'),
+(16, 'SEGA EUROPE LTD', 'Football Manager 2024', 3, 59.99, 'No Football Manager 2024, não basta escolher um esquema tático ou criar uma equipa. Tens de aceitar desafios e ter em contas certas dificuldades  enquanto estabeleces o teu estilo.', 'fotos/1747669225_4.jpg');
 
 -- --------------------------------------------------------
 
@@ -86,7 +126,8 @@ CREATE TABLE `utilizadores` (
 --
 
 INSERT INTO `utilizadores` (`n_utilizador`, `login`, `email`, `passe`, `tipoUtilizador`) VALUES
-(1, 'admin', 'admin@admin.pt', 'admin', 1);
+(1, 'admin', 'admin@admin.pt', '698dc19d489c4e4db73e28a713eab07b', 1),
+(2, 'teste', 'teste@teste.pt', '698dc19d489c4e4db73e28a713eab07b', 2);
 
 -- --------------------------------------------------------
 
@@ -103,6 +144,14 @@ CREATE TABLE `vendas` (
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `carrinho_compra`
+--
+ALTER TABLE `carrinho_compra`
+  ADD PRIMARY KEY (`n_carro`),
+  ADD KEY `n_produto` (`n_produto`,`n_user`),
+  ADD KEY `n_user` (`n_user`);
 
 --
 -- Índices para tabela `plataforma`
@@ -143,16 +192,22 @@ ALTER TABLE `vendas`
 --
 
 --
+-- AUTO_INCREMENT de tabela `carrinho_compra`
+--
+ALTER TABLE `carrinho_compra`
+  MODIFY `n_carro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de tabela `plataforma`
 --
 ALTER TABLE `plataforma`
-  MODIFY `n_plataforma` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `n_plataforma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `n_produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `n_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de tabela `tipouser`
@@ -164,7 +219,7 @@ ALTER TABLE `tipouser`
 -- AUTO_INCREMENT de tabela `utilizadores`
 --
 ALTER TABLE `utilizadores`
-  MODIFY `n_utilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `n_utilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `vendas`
@@ -175,6 +230,13 @@ ALTER TABLE `vendas`
 --
 -- Restrições para despejos de tabelas
 --
+
+--
+-- Limitadores para a tabela `carrinho_compra`
+--
+ALTER TABLE `carrinho_compra`
+  ADD CONSTRAINT `carrinho_compra_ibfk_1` FOREIGN KEY (`n_produto`) REFERENCES `produtos` (`n_produto`),
+  ADD CONSTRAINT `carrinho_compra_ibfk_2` FOREIGN KEY (`n_user`) REFERENCES `utilizadores` (`n_utilizador`);
 
 --
 -- Limitadores para a tabela `produtos`
